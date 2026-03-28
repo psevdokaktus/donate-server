ebfd61e09c796413fbaf959cc54c6194b2eb4ddd
psevdokaktus <kserbinin30@gmail.com>
2026-03-28 17:51:17 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let lastDonation = null;
+
+app.post("/webhook", (req, res) => {
+  lastDonation = req.body;
+  console.log("DONATION:", lastDonation);
+  res.send("ok");
+});
+
+app.get("/events", (req, res) => {
+  res.json(lastDonation || {});
+});
+
+app.listen(3000, () => console.log("Server running"));
