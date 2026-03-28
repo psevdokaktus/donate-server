35ba5a607465fd7b329be32b07a64bfc1d1c5f8e
psevdokaktus <kserbinin30@gmail.com>
2026-03-28 18:07:32 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 let lastDonation = null;
 
 app.post("/webhook", (req, res) => {
